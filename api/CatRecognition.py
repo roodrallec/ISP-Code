72d285366f5b570e9154a37c42229e41323e00ce
Jorge Alexander <jorge@maiv.co.uk>
2019-01-17 16:05:33 +0100

    alpha update

--- api/CatRecognition.py
+++ api/CatRecognition.py
@@ -12,7 +12,7 @@ from imutils import face_utils
 
 API_FOLDER = 'api'
 FOLDER = 'classes'
-ALPHA = 0.4
+ALPHA = 0.02
 # Paths
 PREDICTOR_DAT = os.path.join(API_FOLDER, 'predictor.dat')
 FACE_RECOGNIZER = os.path.join(API_FOLDER, 'lbph_face_recognizer')
